f7368d363fd18097b5b518b99d0ab27c628c97b9
CoreOS Admin <core@gc1.c.reliable-airway-98605.internal>
2015-08-01 19:23:38 +0000

    privileged mode for nagios container.

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@ docker run -d -p 2525:25 -p 8000:80 \
   -v /etc/localtime:/etc/localtime:ro \
   -v /opt/nagios/etc/nagios.cfg:/etc/nagios3/nagios.cfg \
   -v /opt/nagios/etc/conf.d:/etc/nagios3/conf.d \
+  -v /opt/n2n:/opt/n2n \
+  --net=host \
+  --privileged \
   --name nagios \
   siuyin/nagios:20150724 
 
