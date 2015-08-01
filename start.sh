06e9e75c89a13f7024809a8f3b63650436e90dee
CoreOS Admin <core@gc1.c.reliable-airway-98605.internal>
2015-08-01 15:23:11 +0000

    modified:   start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-docker run -d -p 2525:25 -p 80:80 -v /etc/localtime:/etc/localtime:ro --name nagios siuyin/nagios:20150724 
+docker run -d -p 2525:25 -p 80:80 \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /opt/nagios/etc/nagios.cfg:/etc/nagios3/nagios.cfg
+  --name nagios \
+  siuyin/nagios:20150724 
  
