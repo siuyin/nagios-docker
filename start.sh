2bd8811c059f2fa076ca418206ea72cb21af184e
CoreOS Admin <core@gc1.c.reliable-airway-98605.internal>
2015-08-02 03:08:46 +0000

    modified:   start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ docker run -d -p 2525:25 -p 8000:80 \
   -v /opt/nagios/etc/nagios.cfg:/etc/nagios3/nagios.cfg \
   -v /opt/nagios/etc/conf.d:/etc/nagios3/conf.d \
   -v /opt/n2n:/opt/n2n \
-  --net=host \
   --privileged \
   --name nagios \
   siuyin/nagios:20150724 
