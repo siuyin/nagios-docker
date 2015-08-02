a7848a4e66e307db726b031f577122f1e269ada4
CoreOS Admin <core@gc1.c.reliable-airway-98605.internal>
2015-08-02 03:43:51 +0000

    Allow for external secure htpasswd.users .

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 docker run -d -p 2525:25 -p 8000:80 \
   -v /etc/localtime:/etc/localtime:ro \
   -v /opt/nagios/etc/nagios.cfg:/etc/nagios3/nagios.cfg \
+  -v /opt/nagios/etc/htpasswd.users:/etc/nagios3/htpasswd.users \
   -v /opt/nagios/etc/conf.d:/etc/nagios3/conf.d \
   -v /opt/n2n:/opt/n2n \
   --privileged \
